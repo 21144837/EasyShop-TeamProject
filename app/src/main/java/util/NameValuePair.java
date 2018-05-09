a8ffcebd3a4e9434ebf700a21c142b015450d0b2
Rahul <rislam5391@hotmail.co.uk>
2018-05-09 01:59:39 +0100

    EasyShop

---
+++ app/src/main/java/util/NameValuePair.java
@@ -0,0 +1,12 @@
+package util;
+
+
+
+public class NameValuePair {
+    public String name, value;
+
+    public NameValuePair(String _name, String _value) {
+        name = _name;
+        value = _value;
+    }
+}
